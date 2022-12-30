07123375c0ffb5b2ce9d6d3300a4b97c95d8e9d8
Wilmer <wilmer737@users.noreply.github.com>
2022-12-30 01:14:50 -0800

    fix: home page (#3)
    * fix: home page
    
    * test: fix test
    
    * test: fix test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,41 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+// import { useLoaderData } from "@remix-run/react";
+
+import Layout from "~/components/Layout";
+import { requireUserId } from "~/session.server";
+
+const activityTypes = [
+  "DIAPER_CHANGE",
+  "FEEDING",
+  "SLEEP",
+  "BATH",
+  "MEDICATION",
+  "OTHER",
+];
+
+export const loader = async ({ request }: LoaderArgs) => {
+  await requireUserId(request);
+  return json({
+    activityTypes,
+  });
+};
+
 function Home() {
-  return <div>Home</div>;
+  // const data = useLoaderData<typeof loader>();
+
+  return (
+    <Layout title="Today">
+      <div className="flex flex-col items-center justify-center">
+        <img
+          src="ariana_sono.jpg"
+          className="h-40 w-40 rounded-full border-8 border-emerald-600 object-cover"
+          alt="Abel"
+        />
+        <div className="p-2 text-lg">Ariana Guerra</div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe("home test", () => {
   it("should allow you to go to the home page", () => {
     cy.visitAndCheck("/");
-    cy.findByText("Home");
+    cy.findByText("Email address");
   });
 });
