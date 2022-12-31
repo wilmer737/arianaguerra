e57cdeda6c9c2041c50bef371f5a4ffc385cd349
Wilmer Guerra <wilmerguerra@outlook.com>
2022-12-30 22:53:55 -0800

    add cypress

---
+++ app/routes/child/new.tsx
@@ -0,0 +1,9 @@
+function NewChildRoute() {
+  return (
+    <div>
+      <h1>New Child</h1>
+    </div>
+  );
+}
+
+export default NewChildRoute;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,7 +16,6 @@ const activityTypes = [
 
 export const loader = async ({ request }: LoaderArgs) => {
   const user = await requireUser(request);
-  console.log({ user, children: user.children });
 
   if (!user.children || user.children.length === 0) {
     return redirect("/child/new");
@@ -25,6 +24,7 @@ export const loader = async ({ request }: LoaderArgs) => {
   return json({
     // todo:later find a way to select and save the child
     child: user.children[0],
+    activityTypes,
   });
 };
 
@@ -36,10 +36,13 @@ function Home() {
       <div className="flex flex-col items-center justify-center">
         <img
           src={child.imgUrl || "ariana_sono.jpg"}
-          className="rounded-full border-4 border-emerald-600 object-cover h-32 w-32"
+          className="h-32 w-32 rounded-full border-4 border-emerald-600 object-cover"
           alt={fullName}
         />
         <div className="p-2 text-lg">{fullName}</div>
+        <div className="w-full rounded-md border-2 border-red-500 bg-red-400 text-white h-12 py-1 px-2">
+          <h2 className="text-l">Latest Activity</h2>
+        </div>
       </div>
     </Layout>
   );
