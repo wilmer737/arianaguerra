60c8f66c44d355c509098beec60c1c41fed6b0b4
Wilmer Guerra <wilmerguerra@outlook.com>
2023-03-23 15:06:51 -0700

    fix: maybe not sure

--- app/components/forms/activity/form.tsx
+++ app/components/forms/activity/form.tsx
@@ -36,32 +36,32 @@ const metadataFields = {
       options: ["breast", "formula", "solid"],
     },
     {
-      type: 'duration',
-      label: 'Duration',
-      name: 'meta.duration',
+      type: "duration",
+      label: "Duration",
+      name: "meta.duration",
     },
     {
       type: "number",
       label: "Amount",
       name: "meta.amount",
-      unit: 'oz',
-      placeholder: '3'
-    }
+      unit: "oz",
+      placeholder: "3",
+    },
   ],
   [activityTypes.SLEEP]: [
     {
-      type: 'duration',
-      label: 'Duration',
-      name: 'meta.duration',
-    }
+      type: "duration",
+      label: "Duration",
+      name: "meta.duration",
+    },
   ],
   [activityTypes.TUMMY_TIME]: [
     {
-      type: 'duration',
-      label: 'Duration',
-      name: 'meta.duration',
-    }
-  ]
+      type: "duration",
+      label: "Duration",
+      name: "meta.duration",
+    },
+  ],
 };
 
 function ActivityForm({ type }: ActivityFormProps) {
