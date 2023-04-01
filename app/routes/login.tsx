a9b12fea4d333a9a0a569b2fa3561fe3b626fba9
Wilmer Guerra <wilmerguerra@outlook.com>
2023-03-31 20:42:44 -0700

    fix: text colors

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -140,7 +140,7 @@ export default function LoginPage() {
           <input type="hidden" name="redirectTo" value={redirectTo} />
           <button
             type="submit"
-            className="w-full rounded bg-teal-500  py-2 px-4 text-white hover:bg-emerald-600 focus:bg-emerald-400"
+            className="w-full rounded bg-teal-500  py-2 px-4 text-white hover:bg-teal-600 focus:bg-teal-400"
           >
             Log in
           </button>
