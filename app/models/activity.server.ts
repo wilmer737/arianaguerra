d73479a659e1d2aaf44f8ee594eb00ae5bef89f6
Wilmer Guerra <wilmerguerra@outlook.com>
2023-04-03 20:32:42 -0700

    fix: home filter

--- app/models/activity.server.ts
+++ app/models/activity.server.ts
@@ -1,6 +1,6 @@
 import type { Activity } from "@prisma/client";
 import { prisma } from "~/db.server";
-import { startOfDay, endOfDay } from "date-fns";
+import { add } from "date-fns";
 
 export type { Activity } from "@prisma/client";
 
@@ -22,8 +22,8 @@ export async function getActivityByChildId(childId: string, date: Date) {
     where: {
       childId,
       timestamp: {
-        gte: startOfDay(date),
-        lt: endOfDay(date),
+        gte: date,
+        lt: add(date, { days: 1 }),
       },
     },
   });
