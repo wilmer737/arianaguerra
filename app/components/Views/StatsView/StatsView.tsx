36b13c4c780fbb46c11063d5801ad8d301f75447
Wilmer <wilmer737@users.noreply.github.com>
2023-05-01 22:58:23 -0700

    feat: add chart js (#41)

--- app/components/Views/StatsView/StatsView.tsx
+++ app/components/Views/StatsView/StatsView.tsx
@@ -1,23 +1,58 @@
+import React from "react";
+import Chart from "chart.js/auto";
+
 import Layout from "~/components/Layout";
-import Count from "~/components/Views/StatsView/Count";
-import type { CountProps } from "~/components/Views/StatsView/Count";
 
 interface StatsViewProps {
-  counts: CountProps[];
+  counts: { type: string; count: number }[];
+}
+
+interface PieChartProps {
+  id: string;
+  data: number[];
+  labels: string[];
+}
+
+function DoughnutChart({ id, data, labels }: PieChartProps) {
+  const canvasId = `pie-chart-${id}`;
+
+  React.useEffect(() => {
+    const canvasDom = document.getElementById(canvasId);
+    if (!(canvasDom instanceof HTMLCanvasElement)) {
+      return;
+    }
+
+    const dChart = new Chart(canvasDom, {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            data,
+          },
+        ],
+        labels,
+      },
+    });
+
+    return () => {
+      if (dChart) {
+        dChart.destroy();
+      }
+    };
+  }, [canvasId, data, labels]);
+
+  return <canvas id={canvasId} />;
 }
 
 function StatsView({ counts }: StatsViewProps) {
+  const labels = counts.map((c) => c.type.toLowerCase());
+  const data = counts.map((c) => c.count);
+
   return (
     <Layout title="Stats">
-      <div className="flex justify-center">
-        <div className="flex w-auto flex-col justify-center gap-2">
-          {counts.length > 0 ? (
-            counts.map((count) => {
-              return <Count key={count.type} {...count} />;
-            })
-          ) : (
-            <div>No Activity</div>
-          )}
+      <div>
+        <div className="mb-8 bg-white p-8">
+          <DoughnutChart id="count" labels={labels} data={data} />
         </div>
       </div>
     </Layout>
